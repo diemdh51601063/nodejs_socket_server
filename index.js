59771e8f5f0c68424f95aa942817948be18a3360
kieudiem1459 <64761399+kieudiem1459@users.noreply.github.com>
2022-07-22 17:02:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,33 +15,30 @@ var user = {
   info: "test"
 }
 
-var list_user = [];
+var listUser = [];
+var listChecked = [];
 io.on('connection', (socket) => {
   console.log(socket.id);
-  console.log("connected");
-  socket.on('getUserInfo', (socket_id, user_info) => {
-    if (user_info != null) {
-      user_info.socket_id = socket_id;
-      list_user.push(user_info);
+  socket.on('getUserInfo', (socketId, userInfo, allStatusWheel) => {
+    
+    if (userInfo != null) {
+      userInfo.socketId = socketId;
+      listUser.push(userInfo);
     }
-    var flags = [], tmp_list = [];
-    for (let i = 0; i < list_user.length; i++) {
-      if (flags[list_user[i].name]) continue;
-      flags[list_user[i].name] = true;
-      tmp_list.push(list_user[i]);
+    var flags = [], tmpList = [];
+    for (let i = 0; i < listUser.length; i++) {
+      if (flags[listUser[i].name]) continue;
+      flags[listUser[i].name] = true;
+      tmpList.push(listUser[i]);
     }
-    io.local.emit("sendListUser", tmp_list);
-    list_user = tmp_list;
-    console.log(tmp_list);
+    io.local.emit("receiveListUser", tmpList); //HÀM emit sẽ chuyển thông tin đến tất cà các client
+    listUser = tmpList;
+    allStatusWheel.listItem = tmpList;
+    console.log(allStatusWheel);
   })
-  socket.on('chat', (msg) => {
-    console.log('message: ' + msg);
-    socket.send(msg);
 
-    //emit là gửi thông tin đến toàn user có connect đến socket
-    io.local.emit("send", user)
-  });
-  socket.on('myClick', function (data, flag, name) {
+  socket.on('myClick', function (data, flag, name, allStatusWheel) {
+    console.log(allStatusWheel);
 
     //hàm này chuyển thông tin từ 1 socketclient đang thao tác trên màn hình đến những socketclient khác
     socket.broadcast.emit('checkAllItem', data, flag, name);
@@ -57,20 +54,17 @@ io.on('connection', (socket) => {
 
 
   socket.on('disconnect', () => {
-    console.log(socket.id);
     console.log('user disconnected');
+    var pos_ind = listUser.findIndex(i => i.socketId === socket.id);
+    if (pos_ind > -1) {
+      listUser.splice(pos_ind, 1);
+    }
+    var pos_ind = listUser.findIndex(i => i.socketId === undefined);
+    if (pos_ind > -1) {
+      listUser.splice(pos_ind, 1);
+    }
 
-    var pos_ind = list_user.findIndex(i => i.socket_id === socket.id);
-    if (pos_ind > -1) {
-      list_user.splice(pos_ind, 1);
-    }
-    var pos_ind = list_user.findIndex(i => i.socket_id === undefined);
-    if (pos_ind > -1) {
-      list_user.splice(pos_ind, 1);
-    }
-    console.log(list_user);
-    console.log(list_user.length);
-   // io.local.emit("sendListUser", list_user);
+    io.local.emit("sendListUser", listUser);
   });
 });
 
