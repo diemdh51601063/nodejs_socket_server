6afd9a122b1f0d982fe9d4b3c9dfc616982bfc40
kieudiem1459 <64761399+kieudiem1459@users.noreply.github.com>
2022-07-27 17:27:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,71 +5,61 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, { cors: { origin: '*' } });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 let listUser = [];
-let listChecked = [];
 let allStatus = {
   isSpin: false,
   isCheckAll: false,
   listItem: []
 }
+let rand;
 
 io.on('connection', (socket) => {
   socket.on('sendUserAccess', (socketId, userInfo, allStatusWheel) => {
-    allStatus.listItem = allStatusWheel.listItem;
-    //console.log(allStatusWheel.isCheckAll);
-
+    //allStatus.listItem = allStatusWheel.listItem;
     if (userInfo != null) {
       userInfo.socketId = socketId;
       listUser.push(userInfo);
     }
     var flags = [], tmpList = [];
+
     for (let i = 0; i < listUser.length; i++) {
       if (flags[listUser[i].username]) continue;
       flags[listUser[i].username] = true;
-
       if (allStatus.isCheckAll === true && allStatus.isSpin === false) {
         listUser[i].checked = true;
       }
-
       tmpList.push(listUser[i]);
     }
     listUser = tmpList;
 
-    if (allStatus.isSpin === false) {
-      console.log("abc:"+ allStatus.isSpin);
-      socket.broadcast.emit('resetWheelSpin');
-    }
-
-    io.local.emit("receiveListUser", listUser); //HÀM emit sẽ chuyển thông tin đến tất cà các client
-
+    io.local.emit("receiveListUser", listUser, allStatus.isSpin); //hàm local.emit sẽ chuyển thông tin đến tất cà các client
     allStatusWheel.listItem = listUser;
   })
 
   socket.on('clickCheckAllMember', function (isChecked, allStatusWheel) {
     allStatus.isCheckAll = isChecked;
-
-    //hàm này chuyển thông tin từ 1 socketclient đang thao tác trên màn hình đến những socketclient khác
     listUser = allStatusWheel.listItem;
-    socket.broadcast.emit('checkAllMember', isChecked);
+    socket.broadcast.emit('checkAllMember', isChecked); //hàm broadcast.emit chuyển thông tin từ 1 socketclient đang thao tác trên màn hình đến những socketclient khác
   });
 
 
   socket.on('clickCheckOneMember', function (idElement, isChecked, allStatusWheel) {
-
-    //hàm này chuyển thông tin từ 1 socketclient đang thao tác trên màn hình đến những socketclient khác
-    if (isChecked === false) {
-      allStatus.isCheckAll = isChecked;
-    }
+    allStatus = allStatusWheel;
+    // if (isChecked === false) {
+    //   allStatus.isCheckAll = isChecked;
+    // }
+    // console.log(allStatus);
+    // console.log(idElement);
     listUser = allStatusWheel.listItem;
     socket.broadcast.emit('checkOneMember', idElement, isChecked);
   });
 
-
   socket.on('spinWheel', function (random) {
+    rand = random;
     allStatus.isSpin = true;
     socket.broadcast.emit('spinWheelSocket', random);
   })
@@ -79,25 +69,25 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('resetWheelSpin');
   })
 
+  socket.on('completeSpin', () => {
+    allStatus.isSpin = false;
+  })
 
   socket.on('disconnect', () => {
-    //  allStatus.isSpin = false;
-    var pos_ind = listUser.findIndex(i => i.socketId === socket.id);
-    if (pos_ind > -1) {
-      listUser.splice(pos_ind, 1);
+    console.log(socket.id);
+    var posInd = listUser.findIndex(i => i.socketId === socket.id);
+    console.log(posInd);
+    if (posInd > -1) {
+      listUser.splice(posInd, 1);
     }
-    var pos_ind = listUser.findIndex(i => i.socketId === undefined);
-    if (pos_ind > -1) {
-      listUser.splice(pos_ind, 1);
+    var posInd = listUser.findIndex(i => i.socketId === undefined);
+    if (posInd > -1) {
+      listUser.splice(posInd, 1);
     }
     if (allStatus.isSpin === false) {
       allStatus.isSpin = false;
       socket.broadcast.emit('resetWheelSpin');
     }
-
-    console.log(listUser);
-    console.log(allStatus.isSpin);
-
     io.local.emit("receiveListUserDisconnect", listUser, allStatus.isSpin);
   });
 });
